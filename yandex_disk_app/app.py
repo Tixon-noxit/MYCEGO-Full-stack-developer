a2940cfe002b370ff7cdadb38411c3132a1cf57a
Chabusov Tikhon <chabysow@gmail.com>
2024-11-28 20:40:59 +0300

    create index.html && route "/"

--- yandex_disk_app/app.py
+++ yandex_disk_app/app.py
@@ -3,4 +3,36 @@ import requests
 import os
 
 app = Flask(__name__)
-YANDEX_API_BASE_URL = "https://cloud-api.yandex.net/v1/"
+YANDEX_API_BASE_URL = "https://cloud-api.yandex.net/v1/disk/public/resources"
+
+
+
+def get_files_list(public_key: str) -> list:
+    """Подучает список файлов по публичной ссылке."""
+    params = {'public_key': public_key}
+    response = requests.get(YANDEX_API_BASE_URL, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        items = data.get('_embedded', {}).get('items', [])
+        return items
+    else:
+        return []
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    """Маршрут для отображения списка файлов"""
+    files = []
+    if request.method == 'POST':
+        public_key = request.form.get('public_key')
+        files = get_files_list(public_key)
+    return render_template('index.html', files=files)
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
