d5d851976e07b3df64493eeaa37f4f253acfeb7f
Chabusov Tikhon <chabysow@gmail.com>
2024-11-28 20:42:07 +0300

    create download_file()

--- yandex_disk_app/app.py
+++ yandex_disk_app/app.py
@@ -30,8 +30,29 @@ def index():
     return render_template('index.html', files=files)
 
 
+def download_file(public_key: str, file_path: str) -> str:
+    """Скачивает файл по публичной ссылке."""
+    params = {'public_key': public_key, 'path': file_path}
+    response = requests.get(YANDEX_API_BASE_URL + '/download', params=params)
+    
+    if response.status_code == 200:
+        download_url = response.json().get('href')
+        local_filename = file_path.split('/')[-1]
+        file_data = requests.get(download_url)
+        with open(local_filename, 'wb') as f:
+            f.write(file_data.content)
+        return local_filename
+    else:
+        return None
 
-
+@app.route('/download', methods=['POST'])
+def download():
+    public_key = request.form.get('public_key')
+    file_path = request.form.get('file_path')
+    file_name = download_file(public_key, file_path)
+    if file_name:
+        return send_file(file_name, as_attachment=True)
+    return "Ошибка при скачивании файла", 400
 
 
 if __name__ == "__main__":
